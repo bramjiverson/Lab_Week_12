22d38e868a4d6971c56d525874483a831eb8d5c2
Abraham Jiverson Dobiki <amazenour5@gmail.com>
2025-12-06 14:55:56 +0700

    Update MovieService.kt

--- app/src/main/java/com/example/test_lab_week_12/api/MovieService.kt
+++ app/src/main/java/com/example/test_lab_week_12/api/MovieService.kt
@@ -7,6 +7,6 @@ import retrofit2.http.Query
 interface MovieService {
     @GET("movie/popular")
     suspend fun getPopularMovies(
-        @Query("def1e691d83bdeb5e7a808758e56275c") apiKey: String
+        @Query("") apiKey: String
     ): PopularMoviesResponse
 }
